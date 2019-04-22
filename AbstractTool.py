f1eb134d2234be591a74ec148e22e3a741fb7aa0
Isaac Thiessen <ike.thiessen0@gmail.com>
2019-04-22 11:52:42 -0700

    updated user documentation

--- AbstractTool.py
+++ AbstractTool.py
@@ -10,6 +10,10 @@ class AbstractTool(threading.Thread):
             - String
             - Bash command that will be executed.
             - Recommended: Docker, not required.
+            - Recommended for Docker: add the "--rm" parameter to the "docker run"
+                                      command. This will delete the container on exit
+                                      so we dont have a server full of docker containers
+                                      that will never be used.
 
         - self.timeout:
             - Int
@@ -24,6 +28,15 @@ class AbstractTool(threading.Thread):
             - Method
             - Each tool provides output differently therefor each implementation
               of AbstractTool must override self.parse_output()
+            - Must:
+                1. Retrieve data from self.stdout
+                2. Parse that data for meaningful information
+                3. Put data in relevant place
+            - Phases:
+                1. Just put raw data into self.raw_output ( no parsing )
+                2. Retrieve meaningful information from tool output and
+                   put that information in the appropriate locations in
+                   the database
     """
     def __init__(self, tool_name):
         # calling threading's init
