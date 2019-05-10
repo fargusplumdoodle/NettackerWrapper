8caed4506dddd150767075c494e74da8719cb1eb
Isaac Thiessen <ike.thiessen0@gmail.com>
2019-05-09 17:18:47 -0700

    added run file

---
+++ run.py
@@ -0,0 +1,17 @@
+from NettackerTool import NettackerTool
+import time
+
+# YOU CANNOT SCAN LOCALHOST!!! IM PRETTY SURE IT WILL JUST SCAN THE DOCKER CONTAINER THAT ITS IN!
+n = NettackerTool(target='10.0.0.44', scan_options='all',  timeout=10000)
+n.start()
+
+while not n.finished:
+    print('waiting')
+    time.sleep(1)
+
+f = open('output.txt', 'w')
+f.write(n.raw_output)
+f.close()
+
+print(n.raw_output)
+
